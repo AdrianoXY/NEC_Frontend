def9f3f621d1cc5269ec3bfd239b28881fc1e716
AdrianoXY <117019832+AdrianoXY@users.noreply.github.com>
2023-07-26 17:01:28 +0800

    update

--- src/Components/CustomReport/voice.js
+++ src/Components/CustomReport/voice.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import FuzzySet from 'fuzzyset'
 import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
